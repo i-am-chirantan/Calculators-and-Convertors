294b238b5b8206f7f54f584954dd90ce9a9d8c12
i-am-chirantan <acharyya.chirantan@gmail.com>
2022-05-16 16:44:02 +0530

    eval replaced and fractionalcal is  made to work

---
+++ fractionalcal.js
@@ -0,0 +1,70 @@
+function eval() {
+    a = document.getElementById('a').value;
+    b = document.getElementById('b').value;
+    c = document.getElementById('c').value;
+    d = document.getElementById('d').value;
+    a = parseInt(a);
+    b = parseInt(b);
+    c = parseInt(c);
+    d = parseInt(d);
+
+
+    sign = document.getElementById('sign').value;
+    if (c == 0 || d == 0) {
+        document.getElementById('num').innerHTML = "Denominator cannot be zero";
+        document.getElementById('denom').innerHTML = "";
+        document.getElementById('num').style.color = "Red";
+
+    } else if (sign == '+') {
+        num = (a * d) + b * c;
+        denom = c * d;
+        k = gcd(num, denom);
+        num = num / k;
+        denom = denom / k;
+        document.getElementById('num').innerHTML = num;
+        document.getElementById('denom').innerHTML = denom;
+
+    } else if (sign == '-') {
+        num = (a * d) - b * c;
+        denom = c * d;
+        k = gcd(num, denom);
+        num = num / k;
+        denom = denom / k;
+        document.getElementById('num').innerHTML = num;
+        document.getElementById('denom').innerHTML = denom;
+
+    } else if (sign == '*') {
+        num = a * b;
+        denom = c * d;
+        k = gcd(num, denom);
+        num = num / k;
+        denom = denom / k;
+        document.getElementById('num').innerHTML = num;
+        document.getElementById('denom').innerHTML = denom;
+
+    } else if (sign == '/') {
+        const t = b;
+        b = d;
+        d = t;
+        num = a * b;
+        denom = c * d;
+        k = gcd(num, denom);
+        num = num / k;
+        denom = denom / k;
+        document.getElementById('num').innerHTML = num;
+        document.getElementById('denom').innerHTML = denom;
+    }
+}
+
+function clr() {
+    document.getElementById('a').value = '';
+    document.getElementById('b').value = '';
+    document.getElementById('c').value = '';
+    document.getElementById('d').value = '';
+}
+
+function gcd(a, b) {
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
