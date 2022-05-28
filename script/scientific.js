7d1e88f43ee43af81f74c299486e53121ae4ed71
i-am-chirantan <acharyya.chirantan@gmail.com>
2022-05-28 19:22:39 +0530

    Scienific Calc finally made to work

--- script/scientific.js
+++ script/scientific.js
@@ -1,88 +1,20 @@
 function output() {
 
     var k = document.getElementById("screen").value;
-    k = k.replace('+', '%2B');
 
-    if (k.length == 0) {
-        document.getElementById("result").value = "";
-        return;
-    }
-    k = k.replace(/%/g, "*0.01*");
-    if (k.indexOf("/0") != -1) {
+    try {
+
+        document.getElementById('result').innerHTML = math.evaluate(k);
+        document.getElementById('result').style.color = "green";
+    } catch (err) {
         error();
-        return;
     }
-    validparen(k);
-    trimoper(k.charAt(k.length - 1));
-    trimoper(k.charAt(0));
-    endwithnum(k);
+    console.log(math.evaluate(k));
 
-
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Host': 'evaluate-expression.p.rapidapi.com',
-            'X-RapidAPI-Key': 'fffdeffd94msh665c25c03c42d9ep186373jsn3e8d579657bb',
-        }
-    };
-    var url = 'https://evaluate-expression.p.rapidapi.com/?expression='.concat(k);
-    fetch(url, options, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('result').innerHTML = data;
-            document.getElementById('result').style.color = "purple";
-            console.log(url);
-        });
 }
 
-
-function trimoper(pos) {
-    switch (pos) {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '%':
-        case '^':
-            error("green");
-    }
-    return;
-}
-
-function validparen(str) {
-    var stack = [];
-    var i;
-    for (i = 0; i < str.length; i++) {
-        if (str[i] == '(') {
-            stack.push(str[i]);
-        } else if (str[i] == ')') {
-            if (stack.length == 0) {
-                return false;
-            } else {
-                stack.pop();
-            }
-        }
-    }
-    if (stack.length != 0) {
-        error("blue");
-        return;
-    }
-}
-
-function endwithnum(str) {
-    if (isNaN(str.charAt(str.length - 1))) {
-        error("violet");
-        return;
-    }
-}
-
-function error(colour) {
+function error() {
     document.getElementById("result").value = "Invalid Expression";
-    document.getElementById("result").style.color = colour;
+    document.getElementById("result").style.color = "red";
     return;
 }
