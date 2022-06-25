f4c5286e578f530ee6968adc773667666789ba0e
i-am-chirantan <acharyya.chirantan@gmail.com>
2022-06-25 15:13:38 +0530

    loan js fixed

--- script/loancal.js
+++ script/loancal.js
@@ -2,6 +2,10 @@ function loancal() {
     const p = document.getElementById("p").value;
     const t = document.getElementById("t").value;
     const r = document.getElementById("r").value;
+    if (p == "" || t == "" || r == "") {
+        alert("Please fill in all fields");
+        return;
+    }
     interest = (p * (0.01 * r)) / t;
     emi = ((p / t) + interest).toFixed(2);
 
